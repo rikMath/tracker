aaa6edf11be7b9f2416143a2a0a8c677b5dc5771
Henrique Alves Barbosa <henriquealvesbarbosa@MacBook-Pro-de-Henrique.local>
2023-11-01 12:52:31 -0300

    finished

---
+++ src/components/Box.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="box has-text-weight-bold" :sty;e="estilos">
+        <slot></slot>
+    </div>
+</template>
+
+<script lang="ts">
+    import { defineComponent } from 'vue';
+
+    export default defineComponent({
+        name: 'Box',
+        data () {
+            return {
+                estilo: {
+                    'background-color': '#FAF0CA'
+                }
+            }
+        }
+    })
+
+</script>
